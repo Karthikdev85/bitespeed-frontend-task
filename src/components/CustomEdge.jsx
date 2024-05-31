47d2d1dd0e12fb4411b219b2a19e34e3c89a3341
M.Karthiheyan <davidkarthick85@gmail.com>
2024-05-31 10:21:48 +0530

    Fixed delete edge button UI

--- src/components/CustomEdge.jsx
+++ src/components/CustomEdge.jsx
@@ -1,39 +1,40 @@
-import { BezierEdge, getBezierPath, useReactFlow } from "reactflow";
+import {
+  BezierEdge,
+  EdgeLabelRenderer,
+  getBezierPath,
+  useReactFlow,
+} from "reactflow";
 
 export default function CustomEdge(props) {
   const { id, sourceX, sourceY, targetX, sourcePosition, targetPosition } =
     props;
   const { setEdges } = useReactFlow();
-  const [edgePath, labelX, labelY] = getBezierPath({
-    sourceX,
-    sourceY,
-    targetX,
-    sourcePosition,
-    targetPosition,
-  });
+  const [edgePath, labelX, labelY] = getBezierPath(props);
   return (
     <>
-      <BezierEdge {...props} />;
-      {/* <EdgeLabelRenderer style={{ position: "relative" }}>
-          <div
-            style={{
-              position: "absolute",
-              transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
-              fontSize: 12,
-              pointerEvents: "all",
+      <BezierEdge {...props} path={edgePath} />;
+      <EdgeLabelRenderer style={{ position: "relative" }}>
+        <div
+          style={{
+            position: "absolute",
+            transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+            fontSize: 15,
+            pointerEvents: "all",
+            padding: 24,
+          }}
+          className="nodrag nopan"
+        >
+          <button
+            className="edgebutton"
+            onClick={(e) => {
+              setEdges((prevEdges) => prevEdges.filter((e) => e.id !== id));
+              e.stopPropagation();
             }}
           >
-            <button
-              className="edgebutton"
-              onClick={(e) => {
-                setEdges((prevEdges) => prevEdges.filter((e) => e.id !== id));
-                e.stopPropagation();
-              }}
-            >
-              x
-            </button>
-          </div>
-        </EdgeLabelRenderer> */}
+            x
+          </button>
+        </div>
+      </EdgeLabelRenderer>
     </>
   );
 }
